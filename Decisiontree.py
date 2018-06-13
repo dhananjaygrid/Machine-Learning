4364aa910dc5b093011ffce801303674f892184f
dhananjaygrid <dhananjay.grid@hotmail.com>
2018-06-13 14:38:07 +0200

    Update Decisiontree.py

--- Decisiontree.py
+++ Decisiontree.py
@@ -20,9 +20,16 @@ mean_absolute_error(y,predicted_prices) # This error is an example of in-sample 
 #Therefore we need to find MAE on data that hasn't been used for training. This is known as validation data.
 # This point has given rise to the train and test split of data.
 train_X, val_X, train_y, val_y = train_test_split(X,y,random_state = 0)
-new_model = DecisionTreeRegressor()
-new_model.fit(train_X,train_y)
 
-#Now we can get the actual MAE on the predicted test data.
-act_prediction = new_model.predict(val_X)
-print(mean_absolute_error(val_y, act_prediction))
+# The following is to select max leaf nodes in order to avoid underfitting and overfitting
+def get_mae(max_leaf_nodes):
+    new_model = DecisionTreeRegressor(max_leaf_nodes = max_leaf_nodes, random_state=0)
+    new_model.fit(train_X,train_y)
+    act_prediction = new_model.predict(val_X)
+    mae = mean_absolute_error(val_y, act_prediction)
+    return mae
+
+# iterate over a few values of max_leaf_nodes to find out corresponding mae values. The least mae corresponds to the optimal number of max leaf nodes.
+for max_leaf_nodes in [50, 100, 200, 400, 500, 1000]:
+    our_mae = get_mae(max_leaf_nodes)
+    print("The mae is %d for the leaf nodes%d"%(our_mae,max_leaf_nodes) )
